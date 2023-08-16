94572d54182e856457ac4ef481f8e3282f88e4d0
altcode255 <anonte3p5usu@gmail.com>
2023-08-16 16:51:16 -0700

    Fix secret import

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import tweepy
-import json
+import os
 import random
 
-with open("creds.json", "r") as credentials:
-    keys = json.load(credentials)
+import tweepy
 
 client = tweepy.Client(
-    consumer_key=keys["consumer_key"],
-    consumer_secret=keys["consumer_secret"],
-    access_token=keys["access_token"],
-    access_token_secret=keys["access_token_secret"],
+    consumer_key=os.environ["CONSUMER_KEY"],
+    consumer_secret=os.environ["CONSUMER_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
 )
 
 
