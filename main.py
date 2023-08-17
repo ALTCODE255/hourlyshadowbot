d898b9db14c15b6d1be8d5f79928e1a2b73f38fd
altcode255 <anonte3p5usu@gmail.com>
2023-08-16 18:01:56 -0700

    Revert changes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import random
 
+from dotenv import load_dotenv
 import tweepy
 
+load_dotenv()
+
 client = tweepy.Client(
-    consumer_key=os.environ["CONSUMER_KEY"],
-    consumer_secret=os.environ["CONSUMER_SECRET"],
-    access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
+    consumer_key=os.getenv["CONSUMER_KEY"],
+    consumer_secret=os.getenv["CONSUMER_SECRET"],
+    access_token=os.getenv["ACCESS_TOKEN"],
+    access_token_secret=os.getenv["ACCESS_TOKEN_SECRET"],
 )
 
 
