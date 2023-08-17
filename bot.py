c099df0b0a1f5cbacf43f8072817aa02558282e4
altcode255 <anonte3p5usu@gmail.com>
2023-08-16 18:21:32 -0700

    Fix incorrect syntax

--- bot.py
+++ bot.py
@@ -7,10 +7,10 @@ import tweepy
 load_dotenv()
 
 client = tweepy.Client(
-    consumer_key=os.getenv["CONSUMER_KEY"],
-    consumer_secret=os.getenv["CONSUMER_SECRET"],
-    access_token=os.getenv["ACCESS_TOKEN"],
-    access_token_secret=os.getenv["ACCESS_TOKEN_SECRET"],
+    consumer_key=os.getenv("CONSUMER_KEY"),
+    consumer_secret=os.getenv("CONSUMER_SECRET"),
+    access_token=os.getenv("ACCESS_TOKEN"),
+    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
 )
 
 
